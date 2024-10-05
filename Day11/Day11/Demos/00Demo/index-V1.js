677a61a5c4e25c65b16263cf388917336a0da8e3
somesh123-ctrl <55444715+somesh123-ctrl@users.noreply.github.com>
2024-10-05 07:42:21 +0530

    Add files via upload

---
+++ Day11/Day11/Demos/00Demo/index-V1.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+var helper =  http.createServer((request, response)=>
+  { 
+    console.log("you requested for " + request.url);
+    
+    if(request.url == "/employees")
+    {
+        response.write("Some employees details");
+        response.end();
+    }
+    else
+    {
+        response.write("something else");
+        response.end();
+    }
+});
+
+helper.listen(9999, 
+              ()=>{console.log("server started.")})
